482f0904191273bd4a4a7b9fe0566dda879e1c75
Bryan Stansbury <bdstansbury@users.noreply.github.com>
2020-09-15 21:14:48 -0700

    image again

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ import Text from "../components/container.text"
 export default function Home() {
   return (
     <Layout>
-      <img src= "/images/BooksWaterHeader.png" alt= "Books Water Bryan Stansbury Audiobook Narrator"></img>
+      <img src="/images/BooksWaterHeader.png" alt= "Books Water Bryan Stansbury Audiobook Narrator" width="1600" height="531"></img>
       <Text style={{ float: 'left' }}></Text>
     </Layout>
   );
