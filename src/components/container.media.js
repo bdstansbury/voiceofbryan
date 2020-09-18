ff1c6079cf4a436fbb38e6af3c54c51bcde9bcb0
Bryan Stansbury <bdstansbury@users.noreply.github.com>
2020-09-17 18:16:12 -0700

    mediaelement add

--- src/components/container.media.js
+++ src/components/container.media.js
@@ -6,7 +6,7 @@ export default function Media(props, { children }) {
   return (
     <div class= "transparent" style={{ margin: '0.5rem', padding: `1rem` }}>
       <h4 style={{  letterSpacing: '0.4em' }}>{props.mediaHeading}</h4>
-      <Player playerTitle="Fiction, American Southern, Storyteller" playerSrc="../audio/BStansbury-F-M-Am-Southern.mp3" />
+      <Player playerTitle="Fiction, American Southern, Storyteller" playerSrc="../audio/BStansbury-F-M-Am-Southern.ogg" />
       
       {children}
     </div>
--- src/components/player.js
+++ src/components/player.js
@@ -1,12 +1,12 @@
 import React from "react"
+import mediaelement
 
 export default function Player(props, { children }) {
-  return (
+  return (  
     <div style={{ margin: '0rem', padding: `0rem` }}>
       <h6 style={{  letterSpacing: '0.2em' }}>{props.playerTitle}</h6>
       <audio class= "transparentFull" controls style={{ width: '100%', border: "none" }}>
 	      <source src={props.playerSrc} type="audio/mpeg" />
-        <track src="../utils/samples.vtt" />
 	Your browser does not support the audio element.
       </audio>
       {children}
