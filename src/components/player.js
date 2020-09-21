d37fb3c698a808ac4d35e3e4027f0d6d843e4e4b
Bryan Stansbury <bdstansbury@users.noreply.github.com>
2020-09-21 13:22:46 -0700

    linked moved styles.css into styles folder

--- src/components/player.js
+++ src/components/player.js
@@ -1,6 +1,6 @@
 import React from "react"
 import AudioPlayer from 'react-h5-audio-player';
-import 'react-h5-audio-player/lib/styles.css';
+import '../styles/styles.css';
 
 
 export default function Player(props, { children }) {
